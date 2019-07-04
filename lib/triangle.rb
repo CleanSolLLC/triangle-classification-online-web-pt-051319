447ae5e653ba4a5340dc76a71d8b69968a3e3439
Mark May <mamark90@gmail.com>
2019-07-04 13:18:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ class Triangle
       all[1] == all[2] || all[0] == all[2] || all[0] == all[1]
       :isosceles
     elsif
-      all[0] <> all[1] && all[1] <> all[2]
+      all[0] != all[1] && all[1] != all[2]
       :scalene
     elsif
       raise TriangleError
