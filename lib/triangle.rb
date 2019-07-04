7dcac55802290dff7980fb5162532ddb66862ee6
Mark May <mamark90@gmail.com>
2019-07-04 15:07:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -24,10 +24,8 @@ class Triangle
     raise TriangleError if all.any?{|side| side <= 0}
 
     if all[0] +all[1] < all.sort.last
-      begin
         raise TriangleError
-      rescue
-      end
+      rescue TriangleError
 
     #all sides equal
     elsif all[0] == all[1] && all[1] == all[2]
