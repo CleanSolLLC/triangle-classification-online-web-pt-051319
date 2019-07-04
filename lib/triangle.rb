234b48cd67cc5dc33d580c1367cf68c58a9435f4
Mark May <mamark90@gmail.com>
2019-07-04 16:07:06 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -27,10 +27,10 @@ class Triangle
   def kind
     if no_side
         raise TriangleError
-    
+
     elsif all[0] == all[1] && all[1] == all[2] && no_side !=0
       :equilateral
-      
+
     elsif all[1] == all[2] || all[0] == all[2] || all[0] == all[1] && all[0] +all[1] > max_side
       :isosceles
     elsif all[0] != all[1] && all[1] != all[2] && all[0] +all[1] > max_side
