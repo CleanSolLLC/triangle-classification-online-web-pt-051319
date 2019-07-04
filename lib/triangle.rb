047a9ece2e3eca41065be4916aebfd146dac7d79
Mark May <mamark90@gmail.com>
2019-07-04 15:54:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,12 +14,12 @@ class Triangle
     @sides
   end
 
-  def max_side 
+  def max_side
     #longest side of triangle
     long_side = all.sort.last
   end
 
-  def no_side 
+  def no_side
     #not a triange if any side = 0
     all.any?{|side| side <= 0}
   end
@@ -31,10 +31,10 @@ class Triangle
     :scalene if all[0] != all[1] && all[1] != all[2] && all[0] +all[1] > max_side
 
     #call error on sides with 0 or less than 0
-    #call error if sum of 2 sides is less than side num 
-    
+    #call error if sum of 2 sides is less than side num
+
     raise TriangleError if not :equilateral, :isosceles, :scalene
-  
+
   end
 
 
