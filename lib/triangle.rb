8e92671ab1300214872ba239c1d18c6aaad55bc7
Mark May <mamark90@gmail.com>
2019-07-04 15:25:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -22,6 +22,7 @@ class Triangle
     #call error on sides with 0 or less than 0
     #call error if sum of 2 sides is less than side num 3
     raise TriangleError if all.any?{|side| side <= 0}
+    binding.pry
 
     #all sides equal
     if all[0] == all[1] && all[1] == all[2]
