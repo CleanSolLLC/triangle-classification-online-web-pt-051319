1fe0c06ebcca84793e496e455f87be841eac3838
Mark May <mamark90@gmail.com>
2019-07-04 16:00:56 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,15 +25,15 @@ class Triangle
   end
 
   def kind
-    binding.pry
-    :equilateral if all[0] == all[1] && all[1] == all[2] && no_side !=0
-    :isosceles if all[1] == all[2] || all[0] == all[2] || all[0] == all[1] && all[0] +all[1] > max_side
-    :scalene if all[0] != all[1] && all[1] != all[2] && all[0] +all[1] > max_side
-
-    #call error on sides with 0 or less than 0
-    #call error if sum of 2 sides is less than side num
-
-    raise TriangleError if not :equilateral || :isosceles || :scalene
+    if all[0] == all[1] && all[1] == all[2] && no_side !=0
+      :equilateral
+    elsif all[1] == all[2] || all[0] == all[2] || all[0] == all[1] && all[0] +all[1] > max_side
+      :isosceles
+    elsif all[0] != all[1] && all[1] != all[2] && all[0] +all[1] > max_side
+      :scalene
+    else
+      
+    raise TriangleError
 
   end
 
